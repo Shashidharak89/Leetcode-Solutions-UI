ce58628d78cb90b5fe21ce01930b9395a9d65229
Shashidharak89 <shashidharak334@gmail.com>
2025-09-08 22:08:49 +0530

    Header design changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { useLeetCode } from '../context/LeetcodeContext';
+import { FiSun, FiMoon } from 'react-icons/fi';
 import './styles/Header.css';
 
 const Header = () => {
   const { theme, toggleTheme } = useLeetCode();
 
   return (
-    <header className="leetcode-header">
+    <header className={`leetcode-header ${theme}`}>
       <div className="leetcode-header-content">
-        <img 
-          src="https://res.cloudinary.com/dsojdpkgh/image/upload/v1757338892/logo_falrwg.png" 
-          alt="Logo" 
-          className="leetcode-logo"
-        />
-        <h1 className="leetcode-title">My LeetCode Solutions</h1>
+        <div className="leetcode-left">
+          <img 
+            src="https://res.cloudinary.com/dsojdpkgh/image/upload/v1757338892/logo_falrwg.png" 
+            alt="Logo" 
+            className="leetcode-logo"
+          />
+          <h1 className="leetcode-title">My LeetCode Solutions</h1>
+        </div>
         <button 
           onClick={toggleTheme}
           className="leetcode-theme-toggle"
           title={`Switch to ${theme === 'light' ? 'dark' : 'light'} theme`}
         >
-          {theme === 'light' ? '🌙' : '☀️'}
+          {theme === 'light' ? <FiMoon /> : <FiSun />}
         </button>
       </div>
     </header>
